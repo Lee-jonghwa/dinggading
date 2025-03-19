371fcc062fc282fd3f5ef9de5ae0e23158b49316
박준호 <qkrwnsgh71w@gmail.com>
2025-03-19 09:30:29 +0900

    feat. back, front, infra

---
+++ backend/src/main/java/com/mickey/dinggading/HelloController.java
@@ -0,0 +1,20 @@
+package com.mickey.dinggading;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+//
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println(hello);
+        return hello;
+    }
+}

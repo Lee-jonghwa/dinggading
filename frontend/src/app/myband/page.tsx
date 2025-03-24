0e3c6c320af939ba71f5de03b95be904b30e35db
김민수 <kmsu1101@daum.net>
2025-03-24 13:55:49 +0900

    Feat. 프론트엔드 세팅파일 업로드

---
+++ frontend/src/app/myband/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { NextPage } from "next"
+
+const Myband: NextPage = () => {
+
+  return (
+    <div> 
+      <h1> Myband </h1>
+
+    </div>
+  )
+}
+
+export default Myband
---
+++ frontend/src/app/tier/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { NextPage } from "next"
+
+const Tier: NextPage = () => {
+
+  return (
+    <div> 
+      <h1> Tier </h1>
+
+    </div>
+  )
+}
+
+export default Tier

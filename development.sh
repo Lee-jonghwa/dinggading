be9ab25350fa67f3e3eb9085f484c69e17d7d41f
박준호 <qkrwnsgh71w@gmail.com>
2025-03-31 20:05:06 +0900

    Merge. 배포 스크립트 수정

--- development.sh
+++ development.sh
@@ -19,10 +19,30 @@ echo -e "D::::::::::::DDD               GGG::::::GGG:::G     D::::::::::::DDD"
 echo -e "DDDDDDDDDDDDD                     GGGGGG   GGGG     DDDDDDDDDDDDD"
 
 
-# 파일 삭제 명령어
-echo -e "\e[1;34m🔧 Shut Down: 현재 실행 중인 환경을 닫는 중 입니다.. 🔧\e[0m"  # 파란색, 볼드 텍스트
-docker-compose -f docker-compose.dev.local.yaml down
+# 생성할 네트워크 이름
+NETWORK_NAME="ding-ga-ding-network"
 
+# 네트워크 존재 여부 확인
+if docker network ls | grep -q $NETWORK_NAME; then
+  echo "네트워크 '$NETWORK_NAME'가 이미 존재합니다. 넘어갑니다."
+else
+  echo "네트워크 '$NETWORK_NAME'가 존재하지 않습니다. 생성합니다..."
+  # 네트워크 생성 (필요한 옵션 추가 가능)
+  docker network create --driver bridge $NETWORK_NAME
+fi
+
+echo -e "\e[1;34m🔧 DB Shut Down: 현재 실행 중인 DB 환경을 닫는 중 입니다.. 🔧\e[0m"
+
+docker-compose -f docker-compose.dev.db.yaml down
+
+echo -e "\e[1;32m✅ Building: 개발 환경을 구성하는 중 입니다...✅\e[0m"
+
+docker-compose -f docker-compose.dev.db.yaml up -d
+
+# 백엔드 .java 파일 삭제
+echo -e "\e[1;34m🔧 Codegen을 실행합니다. 🔧\e[0m"
+
+./codegen.sh
 
 # 포트 8080을 사용하는 프로세스의 PID 찾기
 PID=$(netstat -ano | grep ":8080" | grep "LISTENING" | awk '{print $5}')
@@ -35,10 +55,11 @@ else
   exit 1
 fi
 
-# 백엔드 .java 파일 삭제
-echo -e "\e[1;34m🔧 Codegen을 실행합니다. 🔧\e[0m"  # 파란색, 볼드 텍스트
-./codegen.sh
+
 
 # 클린업 완료 및 빌드 시작
-echo -e "\e[1;32m✅ Building: 개발 환경을 구성하는 중 입니다...✅\e[0m"  # 초록색, 볼드 텍스트
-docker-compose -f docker-compose.dev.local.yaml up --build
+echo -e "\e[1;32m✅ Building: 개발 환경을 구성하는 중 입니다...✅\e[0m"
+
+docker-compose -f docker-compose.dev.yaml down
+
+docker-compose -f docker-compose.dev.yaml up -d --build

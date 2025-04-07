63ebba8a2774331694a1f675bbb7d0f9927faef4
김나율 <nykim.ssafy2024@gmail.com>
2025-04-07 17:33:49 +0900

    Update. 환경변수 설정 변경

--- backend/src/main/java/com/mickey/dinggading/domain/oauth/service/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/mickey/dinggading/domain/oauth/service/OAuth2AuthenticationSuccessHandler.java
@@ -10,17 +10,17 @@ import com.mickey.dinggading.exception.ExceptionHandler;
 import com.mickey.dinggading.util.JWTUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -39,12 +39,12 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response, Authentication authentication)
-            throws IOException {
+        throws IOException {
 
         log.info("onAuthenticationSuccess 호출");
         MemberPrincipal userPrincipal = (MemberPrincipal) authentication.getPrincipal();
         Member member = memberRepository.findByUsername(userPrincipal.getUsername())
-                .orElseThrow(() -> new ExceptionHandler(ErrorStatus.MEMBER_NOT_FOUND));
+            .orElseThrow(() -> new ExceptionHandler(ErrorStatus.MEMBER_NOT_FOUND));
 
         // 사용자 로그인 시 알림 설정
         subscribe(member.getMemberId());
@@ -53,8 +53,8 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         Token jwtToken = jwtUtil.createAccessToken(member);
         log.info("jwtToken: {}", jwtToken);
 
-        String redirectUri = environment.getProperty("oauth2.redirectUri") == null ?
-                "https://j12e107.p.ssafy.io/login" : environment.getProperty("oauth2.redirectUri");
+        String redirectUri = environment.getProperty("oauth2.frontRedirectUri") == null ?
+            "https://j12e107.p.ssafy.io/login" : environment.getProperty("oauth2.frontRedirectUri");
 
         response.setHeader("Authorization", "Bearer " + jwtToken.getAccessToken());
         log.info("uri {}", redirectUri);
@@ -62,13 +62,13 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         // 토큰과 사용자 정보를 함께 전달
         // URL 파라미터 인코딩
         String redirectUrl = UriComponentsBuilder.fromUriString(redirectUri)
-                .queryParam("token", jwtToken.getAccessToken())
-                .queryParam("loginId", member.getUsername())
-                .queryParam("nickname", member.getNickname())
-                .queryParam("profileImg", member.getProfileImgUrl() != null ? member.getProfileImgUrl() : "")
-                .build()
-                .encode()  // 한 번만 인코딩
-                .toUriString();
+            .queryParam("token", jwtToken.getAccessToken())
+            .queryParam("loginId", member.getUsername())
+            .queryParam("nickname", member.getNickname())
+            .queryParam("profileImg", member.getProfileImgUrl() != null ? member.getProfileImgUrl() : "")
+            .build()
+            .encode()  // 한 번만 인코딩
+            .toUriString();
 
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
